a832a39b01727fdd31dac2f9e924c650ea7e2e58
ridersamt <ridersamt@gmail.com>
2023-02-23 21:01:49 +0530

    added HelloWorld program

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    static String text="Hello world! Welcome!";
+    public static void main(String[] args) {
+        int number;
+
+        number = 12;
+        System.out.println(text);
+        System.out.println(number);
+
+    }
+
+
+}
